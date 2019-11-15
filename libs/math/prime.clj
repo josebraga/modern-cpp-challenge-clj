07f7f6716f2af194bb32a4611471764423a019b5
José Braga <eejbraga@gmail.com>
2019-11-15 18:54:35 +0000

    added 1.5

--- libs/math/prime.clj
+++ libs/math/prime.clj
@@ -15,8 +15,19 @@
 (defn prime? [n]
   (= n (smallest-divisor n)))
 
+(defn sexy-prime? [n]
+  (and (prime? n) (prime? (- n 6))))
+
 (defn prime-less-than? [n]
   (def m (- n 1))
   (if (prime? m)
     m
     (recur m)))
+
+(defn sexy-primes? [n]
+  (when (> n 10)
+    (def m (- n 1))
+    (when (sexy-prime? m)
+      (printf "(%s,%s) " m (- m 6)))
+    (recur m)))
+
--- modern-cpp-01-math.clj
+++ modern-cpp-01-math.clj
@@ -7,27 +7,34 @@
 (load-file "./libs/math/core.clj")
 
 (defn p-gcd [a, b]
-  (println "Greatest common divisor:" a b "=" (gcd a b)))
+  (println a b "->" (gcd a b)))
 
 (defn p-lcm [a, b]
-  (println "Least common multiple:" a b "=" (gcd a b)))
+  (println a b "->" (gcd a b)))
 
 (defn p-prime? [n]
-  (println "Is" n "Prime?" (prime? n)))
+  (println n "->" (prime? n)))
 
 (defn p-prime-less-than? [n]
-  (println "Prime less than" n "=" (prime-less-than? n)))
+  (println n "->" (prime-less-than? n)))
 
+(defn p-sexy-primes? [n]
+  (println n "->")
+  (sexy-primes? n)
+  (println))
+
+(println "\n# 1.2 greatest common divisor?")
 (p-gcd 15 6)
 (p-gcd 45 30)
 (p-gcd 90 60)
 
-
+(println "\n# 1.3 least common multiple?")
 (p-lcm 15 6)
 (p-lcm 45 30)
 (p-lcm 90 60)
 (p-lcm 21 6)
 
+(println "\n# 1.4 is prime?")
 (p-prime? 1)
 (p-prime? 6)
 (p-prime? 7)
@@ -35,7 +42,15 @@
 (p-prime? 150)
 (p-prime? 2287)
 
+(println "\n# 1.4 prime less than?")
 (p-prime-less-than? 8)
 (p-prime-less-than? 152)
 (p-prime-less-than? 2260)
 (p-prime-less-than? 2290)
+
+(println "\n# 1.5")
+(p-sexy-primes? 12)
+(p-sexy-primes? 120)
+(p-sexy-primes? 1200)
+
+(println "\n")
